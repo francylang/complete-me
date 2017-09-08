ca4a2eeb00e6c053d8d1fc203425da2b49eaa8f5
Francy Lang <francy.lang@gmail.com>
2017-09-08 07:56:15 -0600

    removed pseudo-code

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -1,30 +1,30 @@
 import { expect } from 'chai';
 import Node from '../scripts/Node';
 
-describe ('Node functionality', () => {
+describe ('NODE FUNCTIONALITY', () => {
   let node;
 
   beforeEach(() => {
     node = new Node();
-  })
+  });
 
   it('should exist', () => {
     expect(node).to.exist;
-  })
+  });
 
   it('should create an instance of Node', () => {
     expect(node).to.be.an.instanceOf(Node);
-  })
+  });
 
   it('should have a letter property', () => {
     expect(node.letter).to.equal(null);
-  })
+  });
 
   it('should not be a word by default', () => {
     expect(node.isWord).to.equal(false);
-  })
+  });
 
   it('should have no children', () => {
     expect(node.children).to.deep.equal({});
-  })
-})
+  });
+});
